21650538486c640bc89fa06329a87d2e5ba524db
Dark <darktheages@gmail.com>
2025-04-13 19:56:00 +0700

    Add ping command with latency response for both message and chat input

---
+++ src/commands/General/ping.ts
@@ -0,0 +1,18 @@
+import { CommandInteraction, Message, SlashCommandBuilder } from 'discord.js';
+import type Command from '../../structures/Command';
+
+export default <Command>{
+    name: 'ping',
+    description: 'Latency of the bot',
+    applicationCommand: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Latency of the bot'),
+    messageRun: async (msg: Message) => {
+        return msg.reply(`Ping! ${msg.client.ws.ping} ms`);
+    },
+
+    chatInputRun: async (ctx: CommandInteraction) => {
+        ctx.reply(`Ping! ${ctx.client.ws.ping} ms`);
+        return;
+    }
+};
